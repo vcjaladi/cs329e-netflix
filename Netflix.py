75c09aac4ca6f78ae9f938b0d5f9bf8322b077c3
Varun Jaladi <vcjaladi@cs.utexas.edu>
2017-02-13 17:08:22 -0600

    First working eval function

--- Netflix.py
+++ Netflix.py
@@ -10,7 +10,7 @@ from requests import get
 from os import path
 from numpy import sqrt, square, mean, subtract
 
-'''
+
 def create_cache(filename):
     """
     filename is the name of the cache file to load
@@ -30,15 +30,14 @@ def create_cache(filename):
 
     return cache
 
-
 AVERAGE_RATING = 3.60428996442
-ACTUAL_CUSTOMER_RATING = create_cache(
-    "cache-actualCustomerRating.pickle")
+AVERAGE_MOVIE_RATING = create_cache("cache-averageMovieRating.pickle")
+ACTUAL_CUSTOMER_RATING = create_cache("cache-actualCustomerRating.pickle")
+'''
 AVERAGE_MOVIE_RATING_PER_YEAR = create_cache(
     "cache-movieAverageByYear.pickle")
 YEAR_OF_RATING = create_cache("cache-yearCustomerRatedMovie.pickle")
-CUSTOMER_AVERAGE_RATING_YEARLY = create_cache(
-    "cache-customerAverageRatingByYear.pickle")
+CUSTOMER_AVERAGE_RATING_YEARLY = create_cache("cache-customerAverageRatingByYear.pickle")
 '''
 
 actual_scores_cache ={10040: {2417853: 1, 1207062: 2, 2487973: 3}}
@@ -61,18 +60,18 @@ def netflix_eval(reader, writer) :
         if line[-1] == ':':
 		# It's a movie
             current_movie = line.rstrip(':')
-            pred = movie_year_cache[int(current_movie)]
-            pred = (pred // 10) *10
-            prediction = decade_avg_cache[pred]
+            prediction = AVERAGE_MOVIE_RATING[int(current_movie)]
             writer.write(line)
             writer.write('\n')
         else:
 		# It's a customer
             current_customer = line
             predictions.append(prediction)
-            actual.append(actual_scores_cache[int(current_movie)][int(current_customer)])
+            actual_tuple = (int(current_customer),int(current_movie))
+            actual.append(ACTUAL_CUSTOMER_RATING[actual_tuple])
             writer.write(str(prediction)) 
             writer.write('\n')	
+
     # calculate rmse for predications and actuals
     rmse = sqrt(mean(square(subtract(predictions, actual))))
     writer.write(str(rmse)[:4] + '\n')
